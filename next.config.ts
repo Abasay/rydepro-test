7b3b5dceb4b24b4d4d7428527bfbf8906fb6b825
Abasay <abdulsalamasheem@gmail.com>
2024-12-02 12:44:52 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  },
+  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : '',
+  assetPrefix: process.env.URL ? process.env.URL : undefined,
 };
 
 export default nextConfig;
