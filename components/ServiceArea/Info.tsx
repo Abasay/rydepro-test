ca60a7b626103aa57b92557d5af13c2eee4ea4db
Abasay <abdulsalamasheem@gmail.com>
2025-01-09 07:34:39 +0100

    feat: implemented the service page

--- components/ServiceArea/Info.tsx
+++ components/ServiceArea/Info.tsx
@@ -8,35 +8,42 @@ import { ServiceAreas, ServiceAreas1 } from './Regions';
 
 const Info = () => {
   return (
-    <div className=' mt-16 container pb-44 px-20 max-md:px-5 mx-auto border-4 border-green-700 tracking-wider max-md:text-md max-md:leading-6 text-[#0E0E0E]'>
-      <div className='  text-[#3C3C3C] my-12 flex flex-col gap-8 text-xl leading-8 font-[400] max-md:text-md max-md:leading-6'>
-        <p className=' min-w-[200px] max-w-[1048px]'>
-          This global expansion signifies our commitment to excellence and our dedication to redefining passenger
-          transportation worldwide.
-        </p>{' '}
-        <p className=''>Our elite transportation services will soon be available in the following locations:</p>
-        <div className=' flex flex-col gap-16'>
+    <section className='mt-16 w-full pb-44 px-5 md:px-2 lg:px-20 max-md:px-5 mx-auto tracking-wider max-md:text-md max-md:leading-6 text-[#0E0E0E]'>
+      <div className='container mx-auto text-[#3C3C3C] my-12 max-md:my-8 max-430:my-4 mt-10 max-430:mt-0 max-md:mt-4 flex flex-col gap-8 text-xl leading-8 font-[400] max-md:text-md max-md:leading-6'>
+        <article className='flex flex-col gap-6 text-base'>
+          <p className='min-w-[200px] '>
+            This global expansion signifies our commitment to excellence and our dedication to redefining passenger
+            transportation worldwide. <br />
+            Our airport car service guarantees stress-free airport rides, connecting you to your destination on time,
+            every time. With our international chauffeur service, we connect continents one ride at a time.
+          </p>
+          <p>Our elite transportation services will soon be available in the following locations:</p>
+        </article>
+        <div className='flex flex-col gap-16'>
           {ServiceAreas1.map((area, idx) => {
             const { continent, states } = area;
 
             return (
-              <div key={continent} className='flex flex-col gap-8'>
-                <h4 className='text-2xl max-md:text-xl font-bold tracking-wider text-[#0E0E0E]'>
+              <article key={continent} className='flex flex-col gap-8 max-md:gap-6'>
+                <h2 className='text-2xl max-md:text-xl font-bold tracking-wider text-[#0E0E0E]' title={continent}>
                   {continent.toUpperCase()}
-                </h4>
+                </h2>
                 {states.map((state, idx) => {
                   return (
-                    <div className='flex flex-col gap-4' key={state.country}>
-                      <h4 className='text-xl leading-8 max-md:text-md max-md:leading-6 font-bold text-[#0E0E0E] tracking-wider'>
+                    <section key={state.country} className='flex flex-col gap-4'>
+                      <h3
+                        className='text-xl leading-8 max-md:text-lg max-430:text-base font-bold text-[#0E0E0E] tracking-wider'
+                        title={state.country}
+                      >
                         {state.code ? (
-                          <span className=' flex items-center gap-2'>
-                            <Flag code={state.code} width={20} height={20} />
+                          <span className='flex items-center gap-2'>
+                            <Flag code={state.code} width={20} height={20} alt={`Flag of ${state.country}`} />
                             <span>{state.country}</span>
                           </span>
                         ) : (
                           <span>{state.country}</span>
                         )}
-                      </h4>
+                      </h3>
                       <div
                         className={clsx(
                           `flex gap-6 flex-wrap flex-grow max-md:text-md max-md:leading-6`,
@@ -45,36 +52,41 @@ const Info = () => {
                       >
                         {state.regions.map((region, idx) => {
                           return (
-                            <div
+                            <article
                               key={region.region}
-                              className={clsx('flex flex-col gap-2 mt-4  min-w-[180px] ', `${styles['areas-slide']}`)}
+                              className={clsx('flex flex-col gap-2 mt-2 min-w-[188px]', `${styles['areas-slide']}`)}
                             >
-                              <h5 className='text-lg leading-6 max-md:text-md max-md:leading-6 text-[#0E0E0E] font-medium'>
+                              <h4
+                                className='text-lg leading-6 max-md:text-base max-430:text-sm max-md:leading-6 text-[#0E0E0E] font-bold'
+                                title={region.region}
+                              >
                                 {region.code ? (
-                                  <span className=' flex items-center gap-2'>
-                                    <Flag code={region.code} width={20} height={20} /> <span>{region.region}</span>
+                                  <span className='flex items-center gap-2'>
+                                    <Flag code={region.code} width={20} height={20} alt={`Flag of ${region.region}`} />{' '}
+                                    <span>{region.region}</span>
                                   </span>
                                 ) : (
                                   <span>{region.region}</span>
                                 )}
-                              </h5>
-                              <ul
-                                className={clsx(
-                                  'list-none  text-[#0E0E0E] text-[16px] leading-6',
-                                  `${styles['fade-in']}`
-                                )}
-                              >
+                              </h4>
+                              <ul className={clsx('list-none text-[#0E0E0E] text-base', `${styles['fade-in']}`)}>
                                 {region.provinces.map((province, idx) => {
                                   return (
                                     <li
                                       key={province.name}
                                       className={clsx(
-                                        'py-1  max-md:text-md max-md:leading-6 text-[#0E0E0E] font-[400]'
+                                        'py-1 max-md:text-md max-md:leading-6 text-[#0E0E0E] font-normal min-w-fit'
                                       )}
+                                      title={province.name}
                                     >
                                       {province.code ? (
-                                        <span className=' flex items-center gap-2'>
-                                          <Flag code={province.code} width={20} height={20} />{' '}
+                                        <span className='flex items-center gap-2'>
+                                          <Flag
+                                            code={province.code}
+                                            width={20}
+                                            height={20}
+                                            alt={`Flag of ${province.name}`}
+                                          />{' '}
                                           <span>{province.name}</span>
                                         </span>
                                       ) : (
@@ -84,22 +96,25 @@ const Info = () => {
                                   );
                                 })}
                               </ul>
-                            </div>
+                            </article>
                           );
                         })}
                       </div>
-                    </div>
+                    </section>
                   );
                 })}
-              </div>
+              </article>
             );
           })}
-          {ServiceAreas.map((area, idx) => {
+          {/* {ServiceAreas.map((area, idx) => {
             const { continent, regions, code } = area;
 
             return (
-              <div className='flex flex-col gap-8' key={continent}>
-                <h4 className='text-2xl max-md:text-md max-md:leading-6 font-bold text-[#0E0E0E] tracking-wider'>
+              <article className='flex flex-col gap-8' key={continent}>
+                <h2
+                  className='text-2xl max-md:text-md max-md:leading-6 font-bold text-[#0E0E0E] tracking-wider'
+                  title={continent}
+                >
                   {code ? (
                     <span className=' flex items-center gap-2'>
                       <Flag code={code} width={20} height={20} />
@@ -108,8 +123,8 @@ const Info = () => {
                   ) : (
                     <span>{continent.toUpperCase()}</span>
                   )}
-                </h4>
-                <div
+                </h2>
+                <section
                   className={clsx(
                     `flex gap-6 flex-wrap flex-grow max-md:text-md max-md:leading-6`,
                     `${styles['fade-in']}`
@@ -117,11 +132,14 @@ const Info = () => {
                 >
                   {regions.map((region, idx) => {
                     return (
-                      <div
+                      <article
                         key={region.region}
-                        className={clsx('flex flex-col gap-2 mt-4  min-w-[180px] ', `${styles['areas-slide']}`)}
+                        className={clsx('flex flex-col gap-2 mt-2  min-w-[180px] ', `${styles['areas-slide']}`)}
                       >
-                        <h5 className='text-lg max-md:text-md max-md:leading-6 text-[#0E0E0E] font-medium'>
+                        <h3
+                          className='text-lg max-md:text-md max-md:leading-6 text-[#0E0E0E] font-medium'
+                          title={region.region}
+                        >
                           {region.code ? (
                             <span className=' flex items-center gap-2'>
                               <Flag code={region.code} width={20} height={20} />
@@ -130,13 +148,16 @@ const Info = () => {
                           ) : (
                             <span>{region.region}</span>
                           )}
-                        </h5>
+                        </h3>
                         <ul className={clsx('list-none  text-[#0E0E0E] text-[16px] leading-6', `${styles['fade-in']}`)}>
                           {region.provinces.map((province, idx) => {
                             return (
                               <li
                                 key={province.name}
-                                className={clsx('py-1 max-md:text-md max-md:leading-6 text-[#0E0E0E] font-[400]')}
+                                className={clsx(
+                                  'py-1 max-md:text-md max-md:leading-6 text-[#0E0E0E] font-normal min-w-fit'
+                                )}
+                                title={province.name}
                               >
                                 {province.code ? (
                                   <span className=' flex items-center gap-2'>
@@ -150,16 +171,16 @@ const Info = () => {
                             );
                           })}
                         </ul>
-                      </div>
+                      </article>
                     );
                   })}
-                </div>
-              </div>
+                </section>
+              </article>
             );
-          })}
+          })} */}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
