8456a48ffebacee7f59dd9e1a23ba4b57f7f6e30
Abasay <abdulsalamasheem@gmail.com>
2025-01-11 19:50:56 +0100

    fix: fixing accesibility

--- components/Operators/section5.tsx
+++ components/Operators/section5.tsx
@@ -7,29 +7,26 @@ import Image from 'next/image';
 const Section5 = () => {
   return (
     <section className='min-h-[400px] container mx-auto w-full flex justify-center lg:py-10 py-0 pt-0 px-4 md:px-8 lg:px-10 md:mt-6 lg:mt-0 mb-10'>
-      <div className=' flex flex-col md:flex-row justify-between items-center md:min-h-[400px] md:gap-10 lg:gap-10'>
-        <div className='md:w-1/2 w-[335px] max-md:w-full mx-auto flex flex-col gap-6 justify-center h-[400px]'>
-          <div className='flex flex-col gap-2'>
-            <span className='text-base  font-normal text-[#3c3c3c]'>Sign Up</span>
-            <h2 className='text-2xl md:text-[40px] md:leading-[48px] font-medium text-[#0E0E0E]'>Join Today!</h2>
-          </div>
-          <span className=' text-base text-[#3C3C3C] font-medium'>
+      <div className='flex flex-col md:flex-row justify-between items-center md:min-h-[400px] md:gap-10 lg:gap-10'>
+        <article className='md:w-1/2 w-[335px] max-md:w-full mx-auto flex flex-col gap-6 justify-center h-[400px]'>
+          <header className='flex flex-col gap-2'>
+            <h2 className='text-base font-normal text-[#3c3c3c]'>Sign Up</h2>
+            <h3 className='text-2xl md:text-[40px] md:leading-[48px] font-medium text-[#0E0E0E]'>Join Today!</h3>
+          </header>
+          <p className='text-base text-[#3C3C3C] font-medium'>
             Ready to elevate your career? Click the link below to apply now and take the first step toward a more
             fulfilling and lucrative driving experience.
-          </span>
+          </p>
           <Button
             value='Register as an Operator'
             className='bg-[#0E0E0E] py-2 rounded-2xl px-[24px] lg:w-[300px] h-[56px] text-white mt-4 md:mt-0 text-base font-medium'
+            aria-label='Register as an Operator to join RYDEPRO'
           />
-        </div>
-        <div
-          // style={section5Image}
-          className='min-h-[360px] max-h-[560px] max-md:w-full lg:max-w-[636px]  md:max-w-[350px] min-w-[205px] mx-auto mb-10 lg:mb-0'
-        >
+        </article>
+        <div className='min-h-[360px] max-h-[560px] max-md:w-full lg:max-w-[636px] md:max-w-[350px] min-w-[205px] mx-auto mb-10 lg:mb-0'>
           <Image
             src={img}
-            alt='Rydepro Operators'
-            // layout='fill'
+            alt='Illustration of professional drivers working with RYDEPRO'
             width={400}
             height={500}
             placeholder='blur'
